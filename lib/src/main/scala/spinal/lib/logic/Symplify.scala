4039e45f2e33ce1594a36f14be3bfef0335516de
Dolu1990 <charles.papon.90@gmail.com>
2025-09-22 11:47:42 +0200

    lib.logic.Symplify now cache resuts

--- lib/src/main/scala/spinal/lib/logic/Symplify.scala
+++ lib/src/main/scala/spinal/lib/logic/Symplify.scala
@@ -5,6 +5,7 @@ import spinal.lib._
 import spinal.lib.eda.bench.{AlteraStdTargets, Bench, EfinixStdTargets, Rtl, XilinxStdTargets}
 import spinal.lib.eda.xilinx.VivadoFlow
 
+import java.util.concurrent.ConcurrentHashMap
 import scala.collection.mutable
 import scala.math.BigInt
 
@@ -65,8 +66,16 @@ object SymplifyBit {
     null
   }
 
+  private val getPrimeImplicantsByTrueAndFalseCache = new ConcurrentHashMap[(List[Masked], List[Masked], Int), Seq[Masked]]()
+
   // Return primes implicants for the trueTerms, falseTerms spec. Default value is don't care
   def getPrimeImplicantsByTrueAndFalse(trueTerms: Seq[Masked], falseTerms: Seq[Masked], inputWidth : Int): Seq[Masked] = {
+    val key = (trueTerms.toList, falseTerms.toList, inputWidth)
+    val key2 = (trueTerms.toList, falseTerms.toList, inputWidth)
+    if(getPrimeImplicantsByTrueAndFalseCache.containsKey(key)) {
+      return getPrimeImplicantsByTrueAndFalseCache.get(key)
+    }
+
     val startAt = System.nanoTime()
     val primes = mutable.LinkedHashSet[Masked]()
     trueTerms.foreach(_.isPrime = true)
@@ -126,7 +135,10 @@ object SymplifyBit {
     if(reportTime){
       println(s"getPrimeImplicantsByTrueAndFalse $time")
     }
-    primes.toSeq
+
+    val seq = primes.toSeq
+    getPrimeImplicantsByTrueAndFalseCache.put(key, seq)
+    seq
   }
 
   // Verify that the 'terms' doesn't violate the trueTerms ++ falseTerms spec
@@ -141,10 +153,18 @@ object SymplifyBit {
 
   def getPrimeImplicantsByTrue(trueTerms: Seq[Masked], inputWidth : Int) : Seq[Masked] = getPrimeImplicantsByTrueAndDontCare(trueTerms, Nil, inputWidth)
 
+
+  private val getPrimeImplicantsByTrueAndDontCareCache = new ConcurrentHashMap[(List[Masked], List[Masked], Int), Seq[Masked]]()
+
   // Return primes implicants for the trueTerms, default value is False.
   // You can insert don't care values by adding non-prime implicants in the trueTerms
   // Will simplify the trueTerms from the most constrained ones to the least constrained ones
   def getPrimeImplicantsByTrueAndDontCare(trueTerms: Seq[Masked],dontCareTerms: Seq[Masked], inputWidth : Int): Seq[Masked] = {
+    val key = (trueTerms.toList, dontCareTerms.toList, inputWidth)
+    if(getPrimeImplicantsByTrueAndDontCareCache.containsKey(key)) {
+      return getPrimeImplicantsByTrueAndDontCareCache.get(key)
+    }
+
     val startAt = System.nanoTime()
     val primes = mutable.LinkedHashSet[Masked]()
     trueTerms.foreach(_.isPrime = true)
@@ -192,7 +212,10 @@ object SymplifyBit {
     if(reportTime){
       println(s"getPrimeImplicantsByTrueAndDontCare $time")
     }
-    primes.toSeq
+
+    val seq = primes.toSeq
+    getPrimeImplicantsByTrueAndDontCareCache.put(key, seq)
+    seq
   }
 
   def main(args: Array[String]) {
